4e71f1c2cf152d5b575b17c0c4f132796988a283
Andre <andreferreira@live.co.uk>
2023-09-12 11:32:44 +0100

    arrays and objects

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,35 @@
+//? BASICS
 var names = ["Michael", "July"];
 console.log(names);
 var inputs = document.querySelectorAll("input");
 inputs.forEach(function (input) {
     console.log(input);
 });
+function calc(circ) {
+    return circ * Math.PI;
+}
+console.log((calc(2)));
+//? ARRAYS AND OBJECTS
+// -------------------------------------------------
+// arrays
+var names = ['Carl', 'Troy', 'Steve'];
+names.push('Sharma');
+var numbers = [10, 20, 12, 15];
+numbers.push(25);
+var mixed = ['Pierce', 4, 'Abed', 8, 9];
+mixed.push('Marge');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var person = {
+    name: 'Troy',
+    location: 'UK',
+    age: 30
+};
+person.age = 40;
+person.name = 'Marge';
+person = {
+    name: 'Steve',
+    location: 'USA',
+    age: 40,
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,4 @@
+//? BASICS
 const names = ["Michael", "July"];
 
 console.log(names);
@@ -7,3 +8,45 @@ const inputs = document.querySelectorAll("input");
 inputs.forEach(input => {
     console.log(input)
 })
+
+
+function calc(circ: number) {
+    return circ * Math.PI;
+}
+
+console.log((calc(2)))
+
+
+//? ARRAYS AND OBJECTS
+// -------------------------------------------------
+
+// arrays
+let names = ['Carl', 'Troy', 'Steve'];
+
+names.push('Sharma');
+
+let numbers = [10, 20, 12, 15];
+
+numbers.push(25);
+
+let mixed = ['Pierce', 4, 'Abed', 8, 9];
+
+mixed.push('Marge');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let person = {
+  name: 'Troy',
+  location: 'UK',
+  age: 30
+};
+
+person.age = 40;
+person.name = 'Marge';
+
+person = {
+  name: 'Steve',
+  location: 'USA',
+  age: 40,
+};
